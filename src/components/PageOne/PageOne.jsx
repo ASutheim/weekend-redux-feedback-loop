249f7dc1dc84e22ad3bc0a4d8e0dd3a3749cb7db
Anna Sutheim <asutheim@gmail.com>
2023-06-10 15:44:36 -0500

    added comments to clarify

--- src/components/PageOne/PageOne.jsx
+++ src/components/PageOne/PageOne.jsx
@@ -3,10 +3,16 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 function PageOne() {
+  //adds hook to input variable 'feedback'
   let [feedback, setFeedback] = useState("");
+
+  // adds hook for dispatching data to store
   const dispatch = useDispatch();
+
+  // adds hook for routing to another page
   const history = useHistory();
 
+  //sets feedback variable equal to the inputted value
   const handleFeedback = (event) => {
     setFeedback(event.target.value);
   };
@@ -14,10 +20,15 @@ function PageOne() {
   const addFeedback = (event) => {
     event.preventDefault();
     console.log("Sending feelings feedback to store:", feedback);
+
+    //dispatches the feedback to the store when 'submit' field is clicked
     dispatch({
       type: "ADD_TO_FEEDBACK",
+      //structuring the payload to add property name of 'feeling' for later retrieval
       payload: { property: "feeling", value: feedback },
     });
+
+    //routes the user to the next page
     history.push(`/PageTwo`);
   };
 
@@ -25,7 +36,8 @@ function PageOne() {
     <form onSubmit={(event) => addFeedback(event)}>
       <div>
         <label htmlFor="feelings">How are you feeling today?</label>
-
+        {/* structure of input requires the user to choose a value between 1 and 5, value cannot be null*/}
+        {/* hook tracks changes to the input field*/}
         <input
           onChange={handleFeedback}
           id="feelings"
