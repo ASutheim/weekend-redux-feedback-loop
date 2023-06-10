886bba69395f794685d793a4b5c09b33be0e2c3f
Anna Sutheim <asutheim@gmail.com>
2023-06-10 13:40:23 -0500

    Set up index js file with store instance, created component files for each page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+
+
+//Components
+import PageOne from "../PageOne/PageOne.jsx"
 
 function App() {
 
@@ -10,6 +15,12 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Route exact path ="/PageOne">
+          
+        </Route>
+        
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Items in the cart
+const feedback = (state = [], action) => {
+  if (action.type === "ADD_TO_FEEDBACK") {
+    return [...state, action.payload];
+  }
+
+  return state;
+};
+
+const storeInstance = createStore(
+  combineReducers({
+    feedback,
+  }),
+  applyMiddleware(logger)
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
